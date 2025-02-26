1dc4863033dee6280d082a9f80669d74d55fc65a
rajat <agrawal21rajat@gmail.com>
2025-02-26 21:48:07 +0530

    updated code

--- api/submit.js
+++ api/submit.js
@@ -82,12 +82,12 @@ export default async function handler(req, res) {
     const sheetId = process.env.GOOGLE_SHEET_ID;
 
     try {
-      // Prepare team data
+      // Prepare team data from 2D array in form data
       const teamData = [
         [member1Name, member1Contact, member1College],
         [member2Name, member2Contact, member2College],
         [member3Name, member3Contact, member3College],
-        [member4Name, member4Contact, member4College]
+        [member4Name, member4Contact, member4College],
       ].filter(row => row.some(cell => cell)); // Remove empty member entries
 
       console.log('Team data:', teamData);
